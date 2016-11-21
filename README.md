567bf9a521b8382233e85da90242bbc9965e0be7
Leon de Jager <leondejager@gmail.com>
2016-11-21 22:29:57 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Microbadget Notify
+
+Simple go application which can be used in build pipelines to notify microbadger that a new docker image has been pushed.
+
+### Usage
+
+*Drone*
+
+```
+build:
+  image: ldejager/microbadger-notify
+  environment:
+    - MB_REPOSITORY=your/repository
+    - MB_TOKEN=sometoken
+  commands:
+    - microbadger-notify
+  when:
+    status: success
+```
---
+++ main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"bytes"
+	"encoding/xml"
+	"fmt"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+
+	"github.com/kelseyhightower/envconfig"
+
+	try "gopkg.in/matryer/try.v1"
+)
+
+type Microbadger struct {
+	Repository string
+	Token      string
+}
+
+func ConstructedURL() string {
+
+	var args Microbadger
+	err := envconfig.Process("mb", &args)
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	return fmt.Sprintf("https://hooks.microbadger.com/images/%s/%s", args.Repository, args.Token)
+}
+
+func main() {
+
+	if os.Getenv("MB_REPOSITORY") == "" {
+		log.Fatal("You need to provide the repository for Microbadger\n\nUsage: microbadger-notify <repository> <token>")
+		os.Exit(1)
+	}
+
+	if os.Getenv("MB_TOKEN") == "" {
+		log.Fatal("You need to provide the repository for Microbadger\n\nUsage: microbadger-notify <repository> <token>")
+		os.Exit(1)
+	}
+
+	log.Println("Notifying Microbadger...")
+
+	endpoint := fmt.Sprintf(ConstructedURL())
+	empty := url.Values{}
+	values, _ := xml.Marshal(empty)
+	data := bytes.NewBuffer(values)
+
+	var resp *http.Response
+	err := try.Do(func(attempt int) (bool, error) {
+		var err error
+		resp, err = http.Post(endpoint, "application/json", data)
+		return attempt < 2, err
+	})
+
+	if err != nil {
+		log.Fatal("Error occured")
+		os.Exit(1)
+	}
+
+	if (resp.StatusCode) != 200 {
+		log.Fatal("An error occured, check the repository and token provided")
+		os.Exit(1)
+	}
+
+	resp.Body.Close()
+	log.Println("Microbadger notified!")
+}
